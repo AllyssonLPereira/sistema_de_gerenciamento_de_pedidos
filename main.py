728e06de0a5b7a9738528a3f5bc2715eac5ca947
Allysson <allyssonlunguinho.03i@gmail.com>
2025-05-21 17:57:35 -0300

    criando a entidade 'item'

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from cliente import Cliente
+from item import Item
 
 
 cliente = Cliente("Allysson", "Céu")
+item = Item("Bíblia", "100")
+
+
 print(f"Cliente: {cliente.nome}\nEndereço: {cliente.endereco}")
+print(f"Item: {item.nome}\nValor: R${item.preco}")
