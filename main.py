63da49392e9dcb79cf77bbc559474eeda41d4ab4
Allysson <allyssonlunguinho.03i@gmail.com>
2025-05-21 19:00:35 -0300

    criando uma classe para lidar entrega de pedidos

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from pedido.pedido_retirada import PedidoRetirada
+from pedido.pedido_delirevy import PedidoDelivery
 from cliente import Cliente
 from item import Item
-from pedido.pedido_retirada import PedidoRetirada
 
 
 cliente = Cliente("Allysson", "Céu")
+
 item_1 = Item("Bíblia", 100)
 item_2 = Item("Assinatura SOLYD", 1360)
-itens = [item_1, item_2]
+item_3 = Item("Livro 'Black Hat Python - 2° Edição'", 83)
+item_4 = Item("Revival Five", 0)
 
-pedido_retirada = PedidoRetirada(cliente, itens)
-print(f"Preço total: {pedido_retirada.calcular_total():.2f}")
+itens_retirado = [item_1, item_2]
+itens_delirevy = [item_3, item_4]
+
+pedido_retirada = PedidoRetirada(cliente, itens_retirado)
+pedido_delivery = PedidoDelivery(cliente, itens_delirevy, 5)
+
+print(f"Preço total - retirados: {pedido_retirada.calcular_total():.2f}")
+print(f"Preço total - delivery: {pedido_delivery.calcular_total():.2f}")
