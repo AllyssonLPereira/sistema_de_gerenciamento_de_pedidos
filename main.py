fca2341864364d5b774ca25347aed4c079fd0fd0
Allysson <allyssonlunguinho.03i@gmail.com>
2025-05-22 20:31:19 -0300

    criando uma factory para pagamento

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pedido.pedido_retirada import PedidoRetirada
 from pedido.pedido_delirevy import PedidoDelivery
 from pagamento.pagamento_cartao import PagamentoCartao
 from pagamento.pagamento_pix import PagamentoPix
+from pagamento.pagamento_factory import PagamentoFactory
 from cliente import Cliente
 from item import Item
 
@@ -19,4 +20,6 @@ pedido_retirada = PedidoRetirada(cliente, itens_retirado)
 
 valor_pedido = pedido_retirada.calcular_total()
 
-pagamento_cartao = PagamentoCartao().processar(valor_pedido)
+tipo_pagamento = "pix"
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
+pagamento.processar(valor_pedido)
