30adbd02aa141a9a57e24b334bbe46f9e83c2209
Allysson <allyssonlunguinho.03i@gmail.com>
2025-05-22 22:14:36 -0300

    aplicando o padrao 'observer'

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-from pedido.pedido_retirada import PedidoRetirada
-from pedido.pedido_delirevy import PedidoDelivery
-from pagamento.pagamento_factory import PagamentoFactory
 from notificacao.notificacao_facade import NotificacaoFacade
+from observador.observador_status import ObservadorStatus
+from pagamento.pagamento_factory import PagamentoFactory
+from pedido.pedido_delirevy import PedidoDelivery
 from cliente import Cliente
 from item import Item
 
 
+MENSAGEM_PAGO = "O pagamento foi confirmado!"
+MENSAGEM_PREPARANDO = "O pedido está sendo processao!"
+MENSAGEM_ENVIADO = "O pedido saiu para a entrega!"
+
+
 cliente = Cliente("Allysson", "Céu")
 
-item_1 = Item("Bíblia", 100)
-item_2 = Item("Assinatura SOLYD", 1360)
-item_3 = Item("Livro 'Black Hat Python - 2° Edição'", 83)
-item_4 = Item("Revival Five", 0)
+item_1 = Item("Livro 'Black Hat Python - 2° Edição'", 83)
+item_2 = Item("Revival Five", 0)
 
-itens_retirado = [item_1, item_2]
-itens_delivery = [item_3, item_4]
+itens = [item_1, item_2]
 
-pedido_retirada = PedidoRetirada(cliente, itens_retirado)
-pedido_delivery = PedidoDelivery(cliente, itens_delivery, 5)
-
-valor_pedido = pedido_retirada.calcular_total()
+taxa_entrega = 5
+pedido = PedidoDelivery(cliente, itens, taxa_entrega)
 
 tipo_pagamento = "pix"
-pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento)
-pagamento.processar(valor_pedido)
+valor_pedido = pedido.calcular_total()
+pagamento = PagamentoFactory.criar_pagamento(tipo_pagamento).processar(valor_pedido)
 
-MENSAGEM = "Seu pedido saiu para entrega!"
-notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, MENSAGEM)
+pedido.status = "Pedido confirmado!"
+notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, pedido.status)
 
-pedido_delivery.status = "Pedido confirmado!"
-notificacoes = NotificacaoFacade().enviar_notificacoes(cliente, pedido_delivery.status)
+notificacoes = NotificacaoFacade()
+observador = ObservadorStatus(notificacoes)
+pedido.adicionar_observadores(observador)
+
+pedido.status = MENSAGEM_PAGO
+pedido.status = MENSAGEM_PREPARANDO
+pedido.status = MENSAGEM_ENVIADO
